12bd23f3d3e44e9d1dea1865d29fb2328c3fe2e4
navatejaj <125516762+navatejaj@users.noreply.github.com>
2023-05-31 15:19:09 +0530

    Update Addition.java

--- src/com/sathya/Addition.java
+++ src/com/sathya/Addition.java
@@ -3,8 +3,8 @@ package com.sathya;
 public class Addition {
 
 	public static void main(String[] args) {
-		int num1 =10;
-		int num2 = 20;
+		int num1 =100;
+		int num2 = 200;
 		int result;
 		result=(num1+num2);
 		System.out.println("Addition Of Two Numbers"+  result);
